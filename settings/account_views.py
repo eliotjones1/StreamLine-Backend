0e8bc9ce9920471c28aff8a4532ff75055e6b5fd
Eliot Jones <eliot.k.jones@gmail.com>
2023-08-25 10:35:04 -0700

    fixed

--- settings/account_views.py
+++ settings/account_views.py
@@ -124,79 +124,7 @@ class getSubscriptions(generics.ListAPIView):
         out = sorted(out, key=lambda k: k['end_date'])
         return Response(out, status=status.HTTP_200_OK)
 
-class actionItems(generics.ListAPIView):
-    def get(self, request):
-        sessionid = request.COOKIES.get('sessionid')
-        # print(sessionid)
-        # Check if session is active
-        if isSessionActive(sessionid) == False:
-            return Response({'error': 'Session expired'}, status=status.HTTP_400_BAD_REQUEST)
-        user_email = Session.objects.get(session_key=sessionid).get_decoded()['user_email']
-        if user_email is None:
-            return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
-        user = CustomUser.objects.get(email=user_email)
-        sub_status = checkSubscriptionStatus(user)
-        if not sub_status:
-            return Response({'error': 'No StreamLine subscription'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            OUR_RECOMMENDATION = ""
-            today = timezone.now().date()
-            closest_date = ThirdPartySubscription.objects.filter(user=user).aggregate(closest_date=Min('date'))['closest_date']
-            if closest_date is None:
-                return Response({'error': 'User has no subscriptions'}, status=status.HTTP_400_BAD_REQUEST)
-            subscriptions = ThirdPartySubscription.obkects.get(user = user)
-            names = []
-            for subscription in subscriptions:
-                names += subscription.subscription_name
-            closest_sub = ThirdPartySubscription.objects.get(user=user, date=closest_date).subscription_name
-            names.remove(closest_sub)
-            names = set(names)
-            # Get all titles on watchlist
-            watchlist = UserData.objects.get(user=user).media
-            on_closest_sub = []
-            for item in watchlist:
-                services = set(item["streaming_providers"]["flatrate"])
-                if services.intersection(names):
-                    watchlist.remove(item)
-                if closest_sub in services:
-                    on_closest_sub.append(item)
-                    watchlist.remove(item)
-            
-            # Watchlist is now all items not included in current susbcriptions 
-            # on closest sub is all items included in current sub
-            # closest_sub is name of subscription we are handling
-            if len(on_closest_sub) == 0:
-                OUR_RECOMMENDATION = "Delete " + {closest_sub}
-                output = {
-                    "recommendation": OUR_RECOMMENDATION,
-                    "plan": closest_sub
-                }
-                return Response(output, status=status.HTTP_200_OK)
-            cur_prices = modify_input(on_closest_sub)[1]
-            budget = cur_prices[closest_sub]
-            current_ratio = len(on_closest_sub) / budget
 
-            alt_providers, alt_prices, alt_services = modify_input(watchlist)
-            alt_plan = optimize1(alt_providers, alt_prices, alt_services, budget, watchlist)
-            alt_ratio = len(alt_plan["Movies_and_TV_Shows"]) / alt_plan["Price"]
-
-            if current_ratio >= alt_ratio:
-                OUR_RECOMMENDATION = "Keep " + {closest_sub}
-                output = {
-                    "recommendation": OUR_RECOMMENDATION,
-                    "plan": closest_sub
-                }
-                return Response(output, status=status.HTTP_200_OK)
-
-            else:
-                services_out = ', '.join(alt_plan["Services"])
-                OUR_RECOMMENDATION = "Switch to " + services_out + " for " + alt_plan["Price"]
-                output = {
-                    "recommendation": OUR_RECOMMENDATION,
-                    "plan": alt_plan
-                }
-                return Response(output, status=status.HTTP_200_OK)
-            
 @api_view(['POST'])
 def handleSelection(request):
     sessionid = request.COOKIES.get('sessionid')
@@ -384,7 +312,7 @@ class getMyUpcoming(generics.ListAPIView):
 # Need a webhook that tracks the number of days left in a subscription to notify individuals and change tags. 
 
 ## UD #1: show potential services button
-class recommendedServices(models.Model):
+class recommendedServices(generics.ListAPIView):
     def get(self, request):
         sessionid = request.COOKIES.get('sessionid')
         if isSessionActive(sessionid) == False:
@@ -403,3 +331,93 @@ class recommendedServices(models.Model):
             if service not in current_services:
                 output.append(service)
         return Response(output, status=status.HTTP_200_OK)
+
+class blah(generics.ListAPIView):
+    def get(self, request):
+        sessionid = request.COOKIES.get('sessionid')
+        # print(sessionid)
+        # Check if session is active
+        if isSessionActive(sessionid) == False:
+            return Response({'error': 'Session expired'}, status=status.HTTP_400_BAD_REQUEST)
+        user_email = Session.objects.get(session_key=sessionid).get_decoded()['user_email']
+        if user_email is None:
+            return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = CustomUser.objects.get(email=user_email)
+        sub_status = checkSubscriptionStatus(user)
+        if not sub_status:
+            return Response({'error': 'No StreamLine subscription'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            OUR_RECOMMENDATION = ""
+            today = timezone.now().date()
+            closest_date = ThirdPartySubscription.objects.filter(user=user).aggregate(closest_date=Min('date'))['closest_date']
+            if closest_date is None:
+                return Response({'error': 'User has no subscriptions'}, status=status.HTTP_400_BAD_REQUEST)
+            subscriptions = ThirdPartySubscription.obkects.get(user = user)
+            names = []
+            for subscription in subscriptions:
+                names += subscription.subscription_name
+            closest_sub = ThirdPartySubscription.objects.get(user=user, date=closest_date).subscription_name
+            names.remove(closest_sub)
+            names = set(names)
+            # Get all titles on watchlist
+            watchlist = UserData.objects.get(user=user).media
+            on_closest_sub = []
+            for item in watchlist:
+                services = set(item["streaming_providers"]["flatrate"])
+                if services.intersection(names):
+                    watchlist.remove(item)
+                if closest_sub in services:
+                    on_closest_sub.append(item)
+                    watchlist.remove(item)
+            
+            # Watchlist is now all items not included in current susbcriptions 
+            # on closest sub is all items included in current sub
+            # closest_sub is name of subscription we are handling
+            if len(on_closest_sub) == 0:
+                OUR_RECOMMENDATION = "Delete " + {closest_sub}
+                output = {
+                    "recommendation": OUR_RECOMMENDATION,
+                    "plan": closest_sub
+                }
+                return Response(output, status=status.HTTP_200_OK)
+            cur_prices = modify_input(on_closest_sub)[1]
+            budget = cur_prices[closest_sub]
+            current_ratio = len(on_closest_sub) / budget
+
+            alt_providers, alt_prices, alt_services = modify_input(watchlist)
+            alt_plan = optimize1(alt_providers, alt_prices, alt_services, budget, watchlist)
+            alt_ratio = len(alt_plan["Movies_and_TV_Shows"]) / alt_plan["Price"]
+
+            if current_ratio >= alt_ratio:
+                OUR_RECOMMENDATION = "Keep " + {closest_sub}
+                output = {
+                    "recommendation": OUR_RECOMMENDATION,
+                    "plan": closest_sub
+                }
+                return Response(output, status=status.HTTP_200_OK)
+
+            else:
+                services_out = ', '.join(alt_plan["Services"])
+                OUR_RECOMMENDATION = "Switch to " + services_out + " for " + alt_plan["Price"]
+                output = {
+                    "recommendation": OUR_RECOMMENDATION,
+                    "plan": alt_plan
+                }
+                return Response(output, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def actionItems(request):
+    sessionid = request.COOKIES.get('sessionid')
+    # print(sessionid)
+    # Check if session is active
+    if isSessionActive(sessionid) == False:
+        return Response({'error': 'Session expired'}, status=status.HTTP_400_BAD_REQUEST)
+    user_email = Session.objects.get(session_key=sessionid).get_decoded()['user_email']
+    if user_email is None:
+        return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+    user = CustomUser.objects.get(email=user_email)
+    sub_status = checkSubscriptionStatus(user)
+    if not sub_status:
+        return Response({'error': 'No StreamLine subscription'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        pass
