e33f311c9d9735e3726fbf1b45444b9515a12aca
Eliot Jones <eliot.k.jones@gmail.com>
2023-08-25 10:27:42 -0700

    ok?

--- settings/functions.py
+++ settings/functions.py
@@ -1,3 +1,6 @@
+from .models import StreamLineSubscription
+from django.utils import timezone
+
 def store_temp_subs(user_sub):
     temp_subs = {}
     temp_subs['Basic'] = user_sub.Basic
@@ -12,3 +15,23 @@ def store_temp_data(user_data):
     temp_data["bundle"] = user_data.bundle
     temp_data["media"] = user_data.media
     return temp_data
+
+    
+def checkSubscriptionStatus(user):
+    sl_subscription = StreamLineSubscription.objects.get(user = user)
+
+    # Check if subscription basic expiration date was before current time)
+    if sl_subscription.Basic_Expiration is not None and sl_subscription.Basic_Expiration < timezone.now():
+        sl_subscription.Basic = False
+        sl_subscription.Basic_Expiration = None
+        sl_subscription.save()
+        # Check if subscription premium expiration date was before current time)
+    if sl_subscription.Premium_Expiration is not None and sl_subscription.Premium_Expiration < timezone.now():
+        sl_subscription.Premium = False
+        sl_subscription.Premium_Expiration = None
+        sl_subscription.save()
+    if sl_subscription is None or (sl_subscription.Basic == False and sl_subscription.Premium == False):
+        return False
+    return True
+
+
