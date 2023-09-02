a1278ced299e0c7792fe91a99ba95b02238ed95b
Eliot Jones <eliot.k.jones@gmail.com>
2023-09-02 16:19:50 -0700

    i

--- webhooks/admin.py
+++ webhooks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.register(UserStripeCustomer)
-admin.register(UserPaymentInfo)
-admin.register(UserStripePayment)
+admin.site.register(UserStripeCustomer)
+admin.site.register(UserPaymentInfo)
+admin.site.register(UserStripePayment)
