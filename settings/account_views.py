afe10e0569fd64ecf080c7a0a8bd1dfd2d532235
Eliot Jones <eliot.k.jones@gmail.com>
2023-09-02 16:12:03 -0700

    i

--- settings/account_views.py
+++ settings/account_views.py
@@ -119,7 +119,7 @@ class getSubscriptions(generics.ListAPIView):
         subscriptions = ThirdPartySubscription.objects.filter(user=user)
         out = []
         for subscription in subscriptions:
-            if subscription.subscription_status == "Active":
+            if subscription.subscription_status != "Expired":
                 out.append(SubscriptionSerializer(subscription).data)
         # order out by end date soonest to latest
         out = sorted(out, key=lambda k: k['end_date'])
