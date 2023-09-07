0e732b9c42a0e3a86ebe413a8430057250ac1553
Eliot Jones <eliot.k.jones@gmail.com>
2023-09-06 19:07:29 -0700

    i

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,15 +1,11 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import login, register, logout, password_change
+from .views import *
 
-# router = routers.DefaultRouter(trailing_slash=False)
-# router.register('auth', AuthViewSet, basename='auth')
+router = routers.DefaultRouter()
+router.register(r'auth', AuthViewSet, basename='auth')
 
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout, name='logout'),
-    path('change-password/', password_change, name='password_change'),
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
