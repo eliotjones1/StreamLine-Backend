59e78626e26e6e206ccf80202d25b5bc8ab6c45e
Eliot Jones <eliot.k.jones@gmail.com>
2023-09-06 18:29:37 -0700

    admin shit

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.sessions.models import Session
+from django.db import models
+
+
 
 admin.site.register(CustomUser)
+class SessionAdmin(models.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+admin.site.register(Session, SessionAdmin)
 # Register your models here.
