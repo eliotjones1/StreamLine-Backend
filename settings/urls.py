83b9b37ab7bd2e7b8b47b1b1866ea4a8f01ede11
Eliot Jones <eliot.k.jones@gmail.com>
2023-12-31 13:57:20 -0500

    start of VCB

--- settings/urls.py
+++ settings/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('get-user-settings/', ReturnSettings.as_view(), name='user-settings'),
     path('update-user-settings/', UpdateSettings, name='update-settings'),
     path('delete-user-account/', deleteAccount, name='delete-account'),
-    path('subscription/status/', ReturnSettings.as_view(), name='subscription-status'),
+    path('subscription/status/', SubStatus.as_view(), name='subscription-status'),
     path('user-subscriptions/create/', createSubscription, name='create-subscription'),
     path('user-subscriptions/cancel/', removeSubscription, name='remove-subscription'),
     path('user-subscriptions/renew/', renewSubscription, name='renew-subscription'),
@@ -21,5 +21,6 @@ urlpatterns = [
     path('contact/', ContactFormSub, name='contact-us'),
     path('is-authenticated/', isAuthenticated.as_view(), name='is-authenticated'),
     path('user-subscriptions/recommendations/', recommendedServices.as_view(), name='get-recommendations'),
+    path('featured-content/', FeaturedContent.as_view(), name='featured-content'),
 ]
 
