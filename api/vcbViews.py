19537d1ba9d85115a114e1bb8e8eea7c4c077713
Eliot Jones <eliot.k.jones@gmail.com>
2024-01-05 12:05:04 -0500

    uhh idk

--- api/vcbViews.py
+++ api/vcbViews.py
@@ -111,6 +111,7 @@ def getServiceContent(subscription_id, subscription_name, watchlist):
     # In watchlist
     return_dict["In_Watchlist"] = []
     for item in watchlist:
+        print(item)
         streaming_providers = set(item['streaming_providers']['free'] + item['streaming_providers']['ads'] + item['streaming_providers']['flatrate'])
         if subscription_name in streaming_providers:
             return_dict["In_Watchlist"].append(item)
